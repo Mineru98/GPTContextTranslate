0e1d72835ead422f8df26c8b6dba68aea1462541
iamiks <rmstjr1030@naver.com>
2025-02-06 09:51:06 +0900

    Fix : 번역 오류가 있는 경우가 있어 안정화 반영

--- main.py
+++ main.py
@@ -7,23 +7,34 @@ from tqdm import tqdm
 
 
 class Item(BaseModel):
-    origin: str = Field(description="번역 전 입력 값")
-    translated: str = Field(description="번역 후 결과 값")
+    origin_context: str = Field(description="번역 전 [맥락] 입력 값")
+    context_translated: str = Field(description="[맥락] 입력을 [번역 결과 언어]로 번역한 결과 값")
+    origin: str = Field(description="번역 전 [번역할 내용] 입력 값")
+    translated: str = Field(description="[번역할 내용] 입력을 [번역 결과 언어]로 번역한 결과 값")
 
 
-user_prompt_template = """### 맥락
+user_prompt_template = """아래의 입력 데이터를 {target_lang}로 번역하여, 반드시 아래 JSON 형식과 동일하게 출력해 주세요.
 
-{context}
+[입력 데이터]
+{{
+  "origin_context": "{context}",
+  "origin": "{content}"
+}}
 
-### 번역할 내용
+[요청 사항]
+- "origin_context" 항목은 번역 전 맥락입니다.
+- "origin" 항목은 번역 전 내용입니다.
+- 각각을 {target_lang}로 번역하여,
+    - 번역된 맥락은 "context_translated"에,
+    - 번역된 내용은 "translated"에 입력해 주세요.
 
-{content}
-
-### 요청 사항:
- 
-- 주어진 [맥락] 다음에 오는 문장을 {target_lang}로 번역해 주세요.
-- [맥락] 부분도 번역해 주세요.
-- 번역 시 JSON 형식으로 반환해 주세요.
+**반드시 아래 JSON 구조로 출력해 주세요:**
+{{
+  "origin_context": "<번역 전 맥락 원본>",
+  "context_translated": "<번역된 맥락>",
+  "origin": "<번역 전 내용 원본>",
+  "translated": "<번역된 내용>"
+}}
 """
 model_name = "gpt-4o-mini-2024-07-18"
 combined_prompt = PromptTemplate.from_template(user_prompt_template)
@@ -49,6 +60,7 @@ class Cli:
         target_lang: str = "한국어",
         batch: bool = True,
         batch_size: int = 10,
+        key: str = "answer",
     ):
         with open(filename, mode="r", encoding="utf-8") as f:
             rows = orjson.loads(f.read())
@@ -57,15 +69,17 @@ class Cli:
         if batch:
             for i in tqdm(range(0, len(rows), batch_size)):
                 batch = rows[i : i + batch_size]
-                inputs = [{"context": row["prompt"], "content": row["answer"], "target_lang": target_lang} for row in batch]
+                inputs = [{"context": row["prompt"], "content": row[key], "target_lang": target_lang} for row in batch]
                 responses = self.chain.batch(inputs)
                 result.extend([response.model_dump() for response in responses])
         else:
             for i in tqdm(range(0, len(rows), batch_size)):
                 context = rows[i]["prompt"]
-                content = rows[i]["answer"]
+                content = rows[i][key]
                 response = self.chain.invoke({"context": context, "content": content, "target_lang": target_lang})
                 result.append(response.model_dump())
+            with open(output, "w", encoding="utf-8") as f:
+                f.write(orjson.dumps(result, option=orjson.OPT_INDENT_2).decode("utf-8"))
 
         with open(output, "w", encoding="utf-8") as f:
             f.write(orjson.dumps(result, option=orjson.OPT_INDENT_2).decode("utf-8"))
